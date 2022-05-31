c7df18908c6aacb45fb466c5b180eaf779881c5f
y-lily <105120950+y-lily@users.noreply.github.com>
2022-05-31 16:54:16 +0300

    Delete duplicate directory

--- file-io/read-from-file.cpp
+++
@@ -1,9 +0,0 @@
-template <typename T>
-void read_from_file(const std::string& file, std::vector<T>& destination) {
-  std::ifstream input { file };
-  if (!input) { throw std::runtime_error("Can't open" + file); }
-
-  for (T t; input >> t;) {
-    destination.emplace_back(t);
-  }
-}
